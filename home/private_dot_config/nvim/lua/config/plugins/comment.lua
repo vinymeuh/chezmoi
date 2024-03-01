2a6788df6646bebd7f35b322a60c6e33e8675748
vinymeuh <vinymeuh@gmail.com>
2024-03-01 19:57:59 +0100

    nvim: add comment plugin

---
+++ home/private_dot_config/nvim/lua/config/plugins/comment.lua
@@ -0,0 +1,17 @@
+local M = {
+   "numToStr/Comment.nvim",
+   event = false,
+}
+
+function M.config()
+  local wk = require("which-key")
+  wk.register {
+    ["<leader>/"] = { "<Plug>(comment_toggle_linewise_current)", "Comment" },
+  }
+
+  wk.register {
+    ["<leader>/"] = { "<Plug>(comment_toggle_linewise_visual)", "Comment", mode = "v" },
+  }
+end
+
+return M
