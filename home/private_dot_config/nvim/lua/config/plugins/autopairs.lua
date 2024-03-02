d158c74a549d354f14de4ab49f5fa0ac8e56ab91
vinymeuh <vinymeuh@gmail.com>
2024-03-02 10:22:38 +0100

    nvim: add autopairs plugin

---
+++ home/private_dot_config/nvim/lua/config/plugins/autopairs.lua
@@ -0,0 +1,12 @@
+local M = {
+   "windwp/nvim-autopairs",
+   event = "VeryLazy",
+};
+
+M.config = function()
+  require("nvim-autopairs").setup {
+    check_ts = true,  -- TreeSitter
+  }
+end
+
+return M
