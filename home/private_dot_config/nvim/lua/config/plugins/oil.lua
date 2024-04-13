4e24df06191e0573c589bec431bd4e06b978b167
vinymeuh <vinymeuh@gmail.com>
2024-04-13 10:18:34 +0200

    nvim: add oil plugin

---
+++ home/private_dot_config/nvim/lua/config/plugins/oil.lua
@@ -0,0 +1,20 @@
+local M = {
+  "stevearc/oil.nvim",
+};
+
+M.config = function()
+  local wk = require("which-key")
+  wk.register {
+    ["<leader>e"] = { "<cmd>Oil<cr>" , "Explorer" },
+  }
+
+  require("oil").setup({
+    default_file_explorer = true,
+    view_options = {
+      show_hidden = true,
+    },
+  })
+
+end
+
+return M
